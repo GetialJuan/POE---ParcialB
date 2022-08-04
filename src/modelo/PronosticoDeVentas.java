bc8d4e2561c5b1984817900099797edc938d3462
GetialJuan <getial.juan@correounivalle.edu.co>
2022-08-04 14:54:14 -0500

    Base de la clase Pronosicoventas

--- src/modelo/PronosticoDeVentas.java
+++ src/modelo/PronosticoDeVentas.java
@@ -4,10 +4,68 @@
  */
 package modelo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * @author Juan
  */
 public class PronosticoDeVentas {
+    //años - {ventas: ..., delta Y :Yn - Yn-1, porcentajeVariacion: (Yn - Yn-1)/Yn-1
+    private ArrayList<HashMap<String,Float>> años;
+    private float promedioDeVariaciones;
     
+    public PronosticoDeVentas(){
+        años = new ArrayList<>();
+    }
+    
+    public void agregarAño(float ventas){
+        HashMap<String,Float> año = new HashMap<>();
+        if(años.isEmpty()){
+            año.put("ventas", ventas);
+            año.put("deltaVentas", 0f);
+            año.put("porcentajeVariacion", 0f);
+        }
+        else{
+            //datos del años anterior
+            int indiceAño = años.size() -1;
+            float ventasAnterior = años.get(indiceAño).get("ventas");
+            
+            //se crean y añaden los datos del año nuevo
+            float deltaVentas = ventas - ventasAnterior;
+            float porcentajeVariacion = deltaVentas/ventasAnterior;
+            
+            año.put("ventas", ventas);
+            año.put("deltaVentas", deltaVentas);
+            año.put("porcentajeVariacion", porcentajeVariacion);
+        }
+        
+        años.add(año);
+    }
+    
+    public void setPromedioDeVariaciones(){
+        float sumaVariaciones = 0;
+        float nPeriodos = años.size() -1;
+        for(HashMap<String,Float> año : años){
+            sumaVariaciones += año.get("porcentajeVariacion");
+        }
+        
+        promedioDeVariaciones = sumaVariaciones/nPeriodos;
+    }
+    
+    public float promedioDeVariaciones(){
+        return promedioDeVariaciones;
+    }
+    
+    public void borrarAño(int cualAño){
+        if(!años.isEmpty()){
+            años.remove(cualAño);
+        }
+    }
+    
+    public void modificarAño(int cualAño, float ventas){
+        if(años.size() > 1){
+        }
+    }
 }
